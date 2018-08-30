503cba7a20526a104d586c42d9f3acc5bf2736fa
Alejandro_AC <Alejandro_AC@CPD25.hefame.es>
2018-08-30 15:00:43 +0200

    Refactorizado server.js y cambiado el puerto por defecto a 40081

--- server.js
+++ server.js
@@ -5,24 +5,19 @@ mongoose.connect('mongodb://proyman:87654321@hhub1.hefame.es:27017,hhub2.hefame.
 // Carga de modelos
 require('./api/models/pedidosModel');
 
+var app = require('express')();
+app.use(require('body-parser').json({extended: true}));
+app.use(require('morgan')('dev'));
 
-var port = process.env.PORT || 3000;
-var express = require('express');
-
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-
-var app = express();
-app.use(bodyParser.json({extended: true}));
-app.use(morgan('dev'));
-
+var port = process.env.PORT || 40081;
+app.listen(port);
 
 // Carga de rutas
 var routes = require('./api/routes/pedidosRoute');
 routes(app);
 
 
-app.listen(port);
+
 
 
 console.log('Servidor API Consultas Proyman v0.0.1');
